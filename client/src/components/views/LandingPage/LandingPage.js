8ea96ec2ed619e5b0a288aed35a7642511c62ea0
perfumelim <perfumelim@gmail.com>
2021-09-16 22:27:21 +0900

    [ADD] checkbox 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,12 +3,18 @@ import axios from "axios";
 import { Icon, Col, Card, Row } from "antd";
 import Meta from "antd/lib/card/Meta";
 import ImageSlider from "../../utils/ImageSlider";
+import CheckBox from "./Sections/CheckBox";
+import {continents, price} from "./Sections/Datas";
 
 function LandingPage() {
   const [Products, setProducts] = useState([]);
   const [Skip, setSkip] = useState(0)
   const [Limit, setLimit] = useState(8)
   const [PostSize, setPostSize] = useState(0)
+  const [Filters, setFilters] = useState({
+    continents: [],
+    price: []
+  })
 
   useEffect(() => {
     let body = {
@@ -59,6 +65,23 @@ function LandingPage() {
     );
   });
 
+  const showFilteredResults = (filters) => {
+    let body = {
+      skip: 0,
+      limit: Limit,
+      filters: filters
+    }
+
+    getProducts(body)
+    setSkip(0)
+  }
+
+  const handleFilters = (filter, category) => {
+    const newFilters = {...Filters}
+    newFilters[category] = filters
+    showFilteredResults(newFilters)
+  }
+
   return (
     <div style={{ width: "75%", margin: "3rem auto" }}>
       <div style={{ textAlign: "center" }}>
@@ -67,6 +90,12 @@ function LandingPage() {
           <Icon type="rocket" />
         </h2>
       </div>
+      {/*Fiter*/}
+      {/*CheckBox*/}
+      <CheckBox list={continents} handleFilters={filters=> handleFilters(filters, "continents")}/>
+      {/*RadioBox*/}
+      {/*Search*/}
+      {/*Cards*/}
       <Row gutter={[16, 16]}>{renderCards}</Row>
       {PostSize >= Limit &&
        <div style={{ display: "flex", justifyContent: "center" }}>
--- server/routes/product.js
+++ server/routes/product.js
@@ -43,6 +43,15 @@ router.post("/products", (req, res) => {
 
   let limit = req.body.limit ? parseInt(req.body.limit): 50;
   let skip = req.body ? parseInt(req.body.skip) : 0;
+  let findArgs = {};
+
+  for(let key in req.body.filters) {
+    if(req.body.filters[key].length > 0) {
+      findArgs[key] = req.body.filters[key];
+    }
+  }
+
+  console.log('findArgs', findArgs)
 
   Product.find()
     .populate("writer")
