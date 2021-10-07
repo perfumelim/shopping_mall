bdb4e2e3c604be930453d504343bcc68ffebb0da
perfumelim <perfumelim@gmail.com>
2021-10-07 22:19:28 +0900

    [ADD] search 기능 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,57 +4,56 @@ import { Icon, Col, Card, Row, Collapse } from "antd";
 import Meta from "antd/lib/card/Meta";
 import ImageSlider from "../../utils/ImageSlider";
 import CheckBox from "./Sections/CheckBox";
-import {continents, price} from "./Sections/Datas";
+import { continents, price } from "./Sections/Datas";
 import RadioBox from "./Sections/RadioBox";
+import SearchFeature from "./Sections/SearchFeature";
 
 function LandingPage() {
   const [Products, setProducts] = useState([]);
-  const [Skip, setSkip] = useState(0)
-  const [Limit, setLimit] = useState(8)
-  const [PostSize, setPostSize] = useState(0)
+  const [Skip, setSkip] = useState(0);
+  const [Limit, setLimit] = useState(8);
+  const [PostSize, setPostSize] = useState(0);
   const [Filters, setFilters] = useState({
     continents: [],
-    price: []
-  })
+    price: [],
+  });
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     let body = {
       skip: Skip,
-      limit: Limit
-    }
+      limit: Limit,
+    };
 
-    getProducts(body)
-
+    getProducts(body);
   }, []);
 
-  const getProducts = (body)=> {
+  const getProducts = (body) => {
     axios.post("/api/product/products", body).then((response) => {
       if (response.data.success) {
-        if(body.loadMore) {
-        setProducts([...Products, ...response.data.productInfo])
-        } 
-        else {
+        if (body.loadMore) {
+          setProducts([...Products, ...response.data.productInfo]);
+        } else {
           setProducts(response.data.productInfo);
         }
-        setPostSize(response.data.postSize)
+        setPostSize(response.data.postSize);
       } else {
         alert("상품을 가져오는데 실패했습니다");
       }
     });
-  }
+  };
 
   const loadMoreHandler = () => {
-
-    let skip = Skip + Limit
+    let skip = Skip + Limit;
 
     let body = {
       skip: skip,
       limit: Limit,
-      loadMore: true
-    }
-    getProducts(body)
-    setSkip(skip)
-  }
+      loadMore: true,
+    };
+    getProducts(body);
+    setSkip(skip);
+  };
 
   const renderCards = Products.map((product, index) => {
     return (
@@ -70,39 +69,50 @@ function LandingPage() {
     let body = {
       skip: 0,
       limit: Limit,
-      filters: filters
-    }
+      filters: filters,
+    };
 
-    getProducts(body)
-    setSkip(0)
-  }
+    getProducts(body);
+    setSkip(0);
+  };
 
-  const handlePrice = (value)=> {
+  const handlePrice = (value) => {
     const data = price;
     let array = [];
 
     for (let key in data) {
-      if(data[key]._id === parseInt(value, 10)) {
+      if (data[key]._id === parseInt(value, 10)) {
         array = data[key].array;
-
-      } 
+      }
     }
 
     return array;
-  } 
+  };
 
   const handleFilters = (filters, category) => {
-    const newFilters = {...Filters}
-    newFilters[category] = filters
+    const newFilters = { ...Filters };
+    newFilters[category] = filters;
 
-    if(category==="price") {
-      let priceValues = handlePrice(filters)
-      newFilters[category]= priceValues
+    if (category === "price") {
+      let priceValues = handlePrice(filters);
+      newFilters[category] = priceValues;
     }
 
-    showFilteredResults(newFilters)
-    setFilters(newFilters)
-  }
+    showFilteredResults(newFilters);
+    setFilters(newFilters);
+  };
+
+  const updateSearchTerm = (newSearchTerm) => {
+    let body = {
+      skip: 0,
+      limit: Limit,
+      filters: Filters,
+      searchTerm: newSearchTerm,
+    };
+    setSkip(0);
+    getProducts(body);
+    setSearchTerm(newSearchTerm);
+  };
 
   return (
     <div style={{ width: "75%", margin: "3rem auto" }}>
@@ -112,25 +122,40 @@ function LandingPage() {
           <Icon type="rocket" />
         </h2>
       </div>
-      {/*Fiter*/}
-      <Row gutter={[16,16]}>
+      {/*Filter*/}
+      <Row gutter={[16, 16]}>
         <Col lg={12} xs={24}>
-         {/*CheckBox*/}
-      <CheckBox list={continents} handleFilters={filters=> handleFilters(filters, "continents")}/>
+          {/*CheckBox*/}
+          <CheckBox
+            list={continents}
+            handleFilters={(filters) => handleFilters(filters, "continents")}
+          />
         </Col>
         <Col lg={12} xs={24}>
-         {/*RadioBox*/}
-         <RadioBox list={price} handleFilters={filters=> handleFilters(filters, "price")}/>
+          {/*RadioBox*/}
+          <RadioBox
+            list={price}
+            handleFilters={(filters) => handleFilters(filters, "price")}
+          />
         </Col>
-        </Row>
+      </Row>
       {/*Search*/}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          margin: "1rem auto",
+        }}
+      >
+        <SearchFeature refreshFunction={updateSearchTerm} />
+      </div>
       {/*Cards*/}
       <Row gutter={[16, 16]}>{renderCards}</Row>
-      {PostSize >= Limit &&
-       <div style={{ display: "flex", justifyContent: "center" }}>
-       <button onClick={loadMoreHandler}>더보기</button>
-     </div>
-      }
+      {PostSize >= Limit && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button onClick={loadMoreHandler}>더보기</button>
+        </div>
+      )}
     </div>
   );
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -40,40 +40,52 @@ router.post("/", (req, res) => {
 });
 
 router.post("/products", (req, res) => {
-
-  let limit = req.body.limit ? parseInt(req.body.limit): 50;
+  let limit = req.body.limit ? parseInt(req.body.limit) : 50;
   let skip = req.body ? parseInt(req.body.skip) : 0;
   let findArgs = {};
+  let term = req.body.searchTerm;
 
-  for(let key in req.body.filters) {
-    if(req.body.filters[key].length > 0) {
-
-      if(key ==="price") {
+  for (let key in req.body.filters) {
+    if (req.body.filters[key].length > 0) {
+      if (key === "price") {
         findArgs[key] = {
           $gte: req.body.filters[key][0],
-          $lte: req.body.filters[key][1]
-        }
+          $lte: req.body.filters[key][1],
+        };
+      } else {
+        findArgs[key] = req.body.filters[key];
       }
-
-      else {
-        findArgs[key] = req.body.filters[key];   
-      }
-
     }
   }
 
-  console.log('findArgs', findArgs)
+  console.log("findArgs", findArgs);
 
-  Product.find()
-    .populate("writer")
-    .skip(skip)
-    .limit(limit)
-    .exec((err, productInfo) => {
-      if (err) return res.status(400).json({ success: false, err });
-      productInfo.length
-      return res.status(200).json({ success: true, productInfo,
-      postSize: productInfo.length });
-    });
+  if (term) {
+    Product.find(findArgs)
+      .find({ $text: { $search: term } })
+      .populate("writer")
+      .skip(skip)
+      .limit(limit)
+      .exec((err, productInfo) => {
+        if (err) return res.status(400).json({ success: false, err });
+        productInfo.length;
+        return res
+          .status(200)
+          .json({ success: true, productInfo, postSize: productInfo.length });
+      });
+  } else {
+    Product.find(findArgs)
+      .populate("writer")
+      .skip(skip)
+      .limit(limit)
+      .exec((err, productInfo) => {
+        if (err) return res.status(400).json({ success: false, err });
+        productInfo.length;
+        return res
+          .status(200)
+          .json({ success: true, productInfo, postSize: productInfo.length });
+      });
+  }
 });
 
 module.exports = router;
